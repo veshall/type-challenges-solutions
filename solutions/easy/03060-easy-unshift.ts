0b15ed06035fc93defb92c5ebb35979eb8f7ae54
vishal paliwal <vishalpaliwal51@gmail.com>
2024-07-08 23:41:48 +0530

    refactored code

--- solutions/easy/03060-easy-unshift.ts
+++ solutions/easy/03060-easy-unshift.ts
@@ -21,13 +21,13 @@
 type Unshift<T extends unknown[], U> = [U, ...T]
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from '@type-challenges/utils'
+// import type { Equal, Expect } from '@type-challenges/utils'
 
-type cases = [
-  Expect<Equal<Unshift<[], 1>, [1]>>,
-  Expect<Equal<Unshift<[1, 2], 0>, [0, 1, 2]>>,
-  Expect<Equal<Unshift<['1', 2, '3'], boolean>, [boolean, '1', 2, '3']>>,
-]
+// type cases = [
+//   Expect<Equal<Unshift<[], 1>, [1]>>,
+//   Expect<Equal<Unshift<[1, 2], 0>, [0, 1, 2]>>,
+//   Expect<Equal<Unshift<['1', 2, '3'], boolean>, [boolean, '1', 2, '3']>>,
+// ]
 
 /* _____________ Further Steps _____________ */
 /*
